9f91f0e1652479418e2373bf7702c8030caf7ab8
ashwinikmr98-art <ashwinikmr98@gmail.com>
2025-12-05 18:01:00 +0530

    adding main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,12 @@ terraform {
 }
 }
 
+provider "aws" {
+	region = "us-east-1"
+}
+
+resource "aws_instance" "server-1" {
+  instance_type = "t3.micro"
+  ami           = "ami-0ecb62995f68bb549"
+  key_name = "devops"
+}
